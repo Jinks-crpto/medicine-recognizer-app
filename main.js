ff17a70f435478c0041da62dc941ada69f235281
Jinks-crpto <77697885+Jinks-crpto@users.noreply.github.com>
2021-08-10 15:37:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,3 +13,23 @@ Webcam.attach('#camera');
      document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'">';
     });
  }
+ console.log('ml5 version', ml5.version);
+ classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/RX5aAL0eX/model.json',modelLoaded);
+ function modelLoaded(){
+     console.log('Model Loaded!');
+ }
+
+ function check(){
+     img=document.getElementById('selfie_image');
+     classifier.classify(img, gotResult);
+ }
+
+ function gotResult(error,results) {
+     if(error){
+         console.error(error);
+     }else{
+         console.log(results);
+         document.getElementById("result_object_name").innerHTML=results [0].label;
+         document.getElementById("result_object_accuracy").innerHTML=results [0].confidence.toFixed(3);
+     }
+ }
